752e49e821f0c649a795ce11f150f388e895e8f1
omega5300 <omegamatrix5351@gmail.com>
2021-02-22 13:04:26 -0500

    serveless-update

--- src/app.js
+++ src/app.js
@@ -14,7 +14,31 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
 // routes
-app.use('/api', require('./routes'));
+// firebase
+const { firestore } = require('../models/firebase');
+
+// db initialization
+const db = firestore();
+
+// router initialization
+const router = express.Router();
+
+// routes
+
+/* get users */
+router.get('/', async (req, res) => {
+  const news = [];
+  const tpmadAllNews = await db.collection('news').get();
+  if (tpmadAllNews.docs.length > 0) {
+    for (const tpmadNews of tpmadAllNews.docs) {
+      news.push(tpmadNews.data());
+    }
+  }
+  res.json(news);
+});
+
+// routes
+app.use('/api', router);
 
 // exports
 module.exports = app;
